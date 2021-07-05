6050c61569c52a5e40e0b03319d5904fc60252cf
Алёна Царегородцева <aileen.kingsler@mail.ru>
2021-07-05 14:55:14 +1000

    parallax

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,7 +2,26 @@ $(function () {
 
   $('.search-form__toggle').on('click', function () {
     $('.search-form').toggleClass('show');
-  })
+  });
+
+  if ($(window).width() >= 1280) {
+    var $paraElm = $('.section-parallax');
+    parallaxThis($paraElm);
+
+    $(window).on('scroll', function () {
+      parallaxThis($paraElm);
+    });
+
+    function parallaxThis($elm) {
+      $elm.each(function () {
+        var $p = $(this), $pos = $p.offset().top,
+          $bg = $('.section-parallax__bg', $p),
+          $pr = ($pos - $(window).scrollTop()) / ($(window).height() + $p.height()) * 100,
+          $shift = Math.round($bg.height() / 130 * $pr);
+        $bg.css({ marginTop: -$shift });
+      });
+    }
+  }
 
   $('.posts-slider').slick({
     slidesToShow: 4,
